2a25e2b423a669a2a28322592121b3de4226d152
harvey magana <heyharv2012@gmail.com>
2020-08-15 12:35:11 -0700

    Link added to the Home component that will go to the Pizza form.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <>
       <h1>Lambda Eats Home Page</h1>
-      
+      <Link to="/pizza">
+        <div className="home-button">Pizza Form</div>
+      </Link>
     </>
   );
 };
