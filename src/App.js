76384508b99e48e4469bdb6666662311afb2da38
harvey magana <heyharv2012@gmail.com>
2020-08-15 12:23:01 -0700

    Home component added and BrowserRouter added to index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Home from "./components/Home";
 import Pizza from "./components/Pizza";
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
+      <Home />
       <Pizza />
     </>
   );
