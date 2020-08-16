074e1bba097695bab2424dacf456214f3b20ac06
harvey magana <heyharv2012@gmail.com>
2020-08-16 14:55:13 -0700

    Created image direcory under src directory, copies the image from the Assets directory to the images directory, added the image to the HomeImg component, added the HomeImg component to the Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer"; 
+import HomeImg from "./HomeImg"; 
+
 
 const Home = () => {
   return (
     <div>
         <Header />
+        <HomeImg />
+        <Footer />
     </div>
   );
 };
